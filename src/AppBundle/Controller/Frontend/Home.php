8a7f44114b3221f4cb21da629dd6b3cfd376a83e
Barti73 <pablo.garcia.r@gmail.com>
2017-02-11 19:27:58 -0300

    Fix Portal

--- src/AppBundle/Controller/Frontend/Home.php
+++ src/AppBundle/Controller/Frontend/Home.php
@@ -1,15 +1,14 @@
 <?php
 
-namespace AppBundle\Controller\Backend;
+namespace AppBundle\Controller\Frontend;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use AppBundle\Controller\BL\Backend\LoginBL;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Constants\Bue5411Constants;
 
-class News extends Controller
+class Home extends Controller
 {
     var $bl;
     
@@ -18,59 +17,11 @@ class News extends Controller
     }
 
     /**
-     * @Route("/Backend/Login/{notExists}")
+     * @Route("/", name="homepage")
      */
-    public function Login(Request $request, $notExists = null)
+    public function Home(Request $request)
     {
-        //return new response(var_dump($this->container));
-        
-        $this->get('session')->remove('userId');
-        $this->get('session')->remove('userNombre');
-        $this->get('session')->remove('userName');
-        $this->get('session')->remove('userPerfil');
-        
-        $formAction = Bue5411Constants::URL_SITE.Bue5411Constants::REDIRECT_LOGIN_CHECK;
-        return $this->render('Backend/login.html.twig', array('formAction' => $formAction,
-                                                              'notExists' => $notExists));
+        $linkHome = Bue5411Constants::URL_SITE;
+        return $this->render('Frontend/portal.html.twig', array('linkHome' => $linkHome));
     }
-    
-//    public function mainConstructor(Request $request, $method = null, $notExists = null)
-//    {
-//        
-//        
-//        if (!$method) { $method = 'Login'; }
-//        return $this->$method($request, $notExists);
-//    }
-    
-//    public function testMethod()
-//    {
-//        return new Response(var_dump("TEST LoginRender"));
-//    }
-    
-//    
-//    public function LoginCheck($request, $notExists = null)
-//    {
-//        $txtUser = $request->request->get('txtUser');
-//        $txtPass = $request->request->get('txtPass');
-//
-//        $response = $this->bl->validateUser($txtUser, $txtPass);
-//        //return new response(var_dump($response));
-//        if ($response)
-//        {
-//            $session = $request->getSession();
-//            $session->set('userId', $response['userId']);
-//            $session->set('userNombre', $response['userNombre']);
-//            $session->set('userName', $response['userName']);
-//            $session->set('userPerfil', $response['userPerfil']);
-//            return $this->redirect(HeroGymConstants::URL_SITE.HeroGymConstants::REDIRECT_CONTROL_ACCESO);
-//        }
-//        else
-//        {
-//            $request->getSession()->invalidate(1);
-//            $notExists = "1";
-//            return $this->Login($request, $notExists);
-//        }
-//    }
-    
-    
 }
